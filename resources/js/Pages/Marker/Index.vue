165b0d65ae727a7f299a9a4ac7ecd1cd492f1235
spitzer77 <evolution9@i.ua>
2023-06-25 21:28:40 +0300

    v1.01 beta with countdown

--- resources/js/Pages/Marker/Index.vue
+++ resources/js/Pages/Marker/Index.vue
@@ -12,9 +12,7 @@
                         rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                id="grid-last-name" type="text" placeholder="00.0000000">
                         <p id="error1" class="text-red-500 text-xs italic mb-3"></p>
-<!--                        31.5055721-->
 <!--                        <p v-if="errors_axios.longitude" class="text-red-500 text-xs italic mb-3">{{ errors_axios.longitude }}</p>-->
-
                     </div>
 
                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -24,28 +22,19 @@
                                 hover:border-gray-500 rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white"
                                 id="grid-first-name" type="text" placeholder="00.0000000">
                         <p id="error2" class="text-red-500 text-xs italic mb-3"></p>
-<!--                        50.8050523-->
 <!--                        <p v-if="this.errors_axios.latitude" class="text-red-500 text-xs italic mb-3">{{ errors.latitude }}</p>-->
-
                     </div>
 
-                    <div class="w-full md:w-1/2 px-3">
+                    <div class="w-full md:w-3/4 px-3">
                         <button class="flex-shrink-0 mt-2 bg-teal-500 hover:bg-teal-700 border-teal-500
                         hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">Set coordinate</button>
+                        <span class="ml-3 text-black-600" id="time"></span>
                     </div>
                 </div>
-
             </form>
         </div>
 
-<!--        <div v-if="markers.length > 0">-->
-<!--            <div v-for="marker in markers">-->
-<!--                Id {{ marker.id }}, Latitude: {{ marker.latitude }}, Longitude {{ marker.longitude }}<br>-->
-<!--            </div>-->
-<!--        </div>-->
-
         <div id="map" style="width: 100%; height: 500px;"></div>
-        <pre id="info"></pre>
     </div>
 </template>
 
@@ -69,66 +58,95 @@ export default {
             center: [30.52428, 50.45056],
             zoom: 9
         });
-
-        // const coordinates = [
-        //     { lng: 31.5055721, lat: 50.8450523 },
-        //     { lng: 31.5055721, lat: 50.8050523 },
-        // ];
     },
 
     data() {
         return {
             latitude: null,
             longitude: null,
-            errors_axios: null
+            interval: 30000, // interval for marker
+            //errors_axios: null
         }
     },
 
     created() {
         window.Echo.channel('store_marker')
             .listen('.store_marker', res => {
-                this.markers.unshift(res.marker)
-
-                const marker = new mapBoxGL.Marker({
-                    color: "#000000",
-                    draggable: false,
-                })
-                    .setLngLat([res.marker.longitude, res.marker.latitude])
-                    .addTo(this.map);
-
-                    setTimeout(() => {
-                        marker.remove();
-                    }, 60000);
+                //this.markers.unshift(res.marker)
+                this.countDown(this.getInterval(), 'time');
+                this.setMarker(res.marker);
             })
     },
 
     methods: {
+        getInterval(){
+            //return 60000; // millisecond
+            return this.interval; // millisecond
+        },
+        countDown(interval, elementId)
+        {
+            const addMilliseconds = (date, milliseconds) => {
+                const result = new Date(date);
+                result.setMilliseconds(result.getMilliseconds() + milliseconds);
+                return result;
+            };
+
+            const countDownDate = addMilliseconds(new Date(), interval);
+
+            let x = setInterval(function() {
+                let now = new Date().getTime();
+                let distance = countDownDate - now;
+
+                let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                if (document.getElementById("time")) {
+                    document.getElementById("time").innerHTML =
+                        'Time to show: ' +
+                        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+                    if (distance < 0) {
+                        clearInterval(x);
+                        //document.getElementById("time").innerHTML = "EXPIRED";
+                        document.getElementById(elementId).innerHTML = "";
+                    }
+
+                    if (distance < 10000) document.getElementById("time").className = 'ml-3 text-red-600';
+                }
+            }, 1000);
+        },
+        setMarker(res){
+            const marker = new mapBoxGL.Marker({
+                color: "#000000",
+                draggable: false,
+            })
+                //.setLngLat([res.data.longitude, res.data.latitude])
+                .setLngLat([res.longitude, res.latitude])
+                .addTo(this.map);
+
+            setTimeout(() => {
+                marker.remove();
+            }, this.getInterval());
+        },
         store() {
             //this.$inertia.post('/markers', {latitude: this.latitude, longitude: this.longitude});
             axios.post('/markers', {latitude: this.latitude, longitude: this.longitude})
                 .then(res => {
 
+                    // for display list of data with current form data, unused in this release
                     //this.markers.unshift(res.data);
                     //this.markers.push(res.data);
 
-                    const marker = new mapBoxGL.Marker({
-                        color: "#000000",
-                        draggable: false,
-                    })
-                        .setLngLat([res.data.longitude, res.data.latitude])
-                        .addTo(this.map);
-
-                    setTimeout(() => {
-                        marker.remove();
-                    }, 60000);
+                    this.countDown(this.getInterval(), 'time');
+                    this.setMarker(res.data);
 
                     this.latitude = null;
                     this.longitude = null;
-
-                    document.getElementById("error1").innerText = '';
-                    document.getElementById("error2").innerText = '';
                 })
                 .catch(error => {
+                    document.getElementById("time").innerHTML = "";
                     //console.log(error.response.data.errors.latitude);
                     let error_long = error.response.data.errors.longitude;
                     let error_lat = error.response.data.errors.latitude;
@@ -146,26 +164,11 @@ export default {
                     else if (error_lat) {
                         document.getElementById("error2").innerText = error.response.data.errors.latitude;
                     }
-                })
-            ;
+                });
         }
     }
 }
 </script>
 
 <style scoped>
-/*#info {*/
-/*    display: table;*/
-/*    position: relative;*/
-/*    margin: 0px auto;*/
-/*    word-wrap: anywhere;*/
-/*    white-space: pre-wrap;*/
-/*    padding: 10px;*/
-/*    border: none;*/
-/*    border-radius: 13px;*/
-/*    font-size: 12px;*/
-/*    text-align: center;*/
-/*    color: #222;*/
-/*    background: #fff;*/
-/*}*/
 </style>
